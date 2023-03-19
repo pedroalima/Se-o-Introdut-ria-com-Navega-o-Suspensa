a473947f1f69bcab6d1b8383b005522bdfa1641d
Pedro Lima <tsc.pedrolima@gmail.com>
2023-03-19 18:07:26 -0300

    Add dynamic desktop menu, submenu and active elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+//      Mobile   
+
+//      Menu mobile
+
 const openMenuButton = document.querySelector("#open-menu");
 const closeMenuButton = document.querySelector("#close-menu");
 const menu = document.querySelector(".mobile__menu");
@@ -11,6 +15,8 @@ const toggleMenu = () => {
 [openMenuButton, closeMenuButton, fade].forEach((elementos) => {
     elementos.addEventListener("click", () => toggleMenu())
 })
+
+//      Submenu mobile
 
 const featureButton = document.querySelector("#feature");
 const listaFeature = document.querySelector("#lista-feature");
@@ -42,4 +48,39 @@ function buttonSubmenuCompany() {
 
 companyButton.addEventListener("click", (evento) => {
     buttonSubmenuCompany();
+})
+
+
+//    Submenu  Desktop
+
+const featureButtonDesktop = document.querySelector("#feature-desktop");
+const listaFeatureDesktop = document.querySelector("#lista-feature-desktop");
+const companyButtonDesktop = document.querySelector("#company-desktop");
+const listaCompanyDesktop = document.querySelector("#lista-company-desktop");
+
+
+function buttonSubmenuFeatureDesktop() {
+    if (listaFeatureDesktop.classList.contains("off")) {
+        listaFeatureDesktop.classList.remove("off");
+    }
+    else {
+        listaFeatureDesktop.classList.add("off");
+    }
+}
+
+featureButtonDesktop.addEventListener("click", (evento) => {
+    buttonSubmenuFeatureDesktop();
+})
+
+function buttonSubmenuCompanyDesktop() {
+    if (listaCompanyDesktop.classList.contains("off")) {
+        listaCompanyDesktop.classList.remove("off");
+    }
+    else {
+        listaCompanyDesktop.classList.add("off");
+    }
+}
+
+companyButtonDesktop.addEventListener("click", (evento) => {
+    buttonSubmenuCompanyDesktop();
 })
